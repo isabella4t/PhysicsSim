c23fcaeb759cc86dac3a02066d6a9ccd85f15fce
Izzy T <itang2023@kehillahstudent.org>
2022-12-18 21:55:40 -0800

    updated for 3 bodies

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,67 @@ import com.sun.xml.internal.ws.wsdl.writer.document.Part;
 
 public class Main {
 
+
     public static void main(String[] args) {
-        System.out.println("Welcome to the simulation");
+        test3System(); //I just want to see if the whole thing complies
+        test2System();
+    }
 
-        int time = 0;
-        double MyPosition = 0;
+    public static void test3System(){
+        System.out.println("Welcome to the simulation w/ 3 particles");
 
-        Particle nee = new Particle(1,0);
+        Particle nee = new Particle(10,0,"jim");
+        Particle noo = new Particle(10,20,"frank");
+        Particle tr = new Particle(10,10,"jane");
+        double time = 0;
 
-        while (!nee.crash) {
 
-            nee.UpdateState();
-            System.out.println(nee.toString());
+        while (!nee.crash && !noo.crash) {
+
+            nee.force = calcForce(nee,noo);
+            nee.force += calcForce(nee,tr);
+            noo.force = (calcForce(noo,nee));
+            noo.force += (calcForce(noo,tr));
+            tr.force = (calcForce(tr,nee));
+            tr.force +=(calcForce(tr,noo));
+
+
+
+            nee.UpdateState(time);
+            noo.UpdateState(time);
+            tr.UpdateState(time);
 
             time++;
+
         }
         System.out.println("The end is upon us");
+    }
+
+    public static void test2System(){
+        System.out.println("Welcome to the simulation w/ 2 particles");
+
+        Particle nee = new Particle(10,-1,"jim");
+        Particle noo = new Particle(10,1,"frank");
+        double time = 0;
+
+
+        while (!nee.crash && !noo.crash) {
+
+            nee.force = calcForce(nee,noo);
+            noo.force = (calcForce(noo,nee));
+
+            nee.UpdateState(time);
+            noo.UpdateState(time);
+
+            time++;
+
+        }
+        System.out.println("The end is upon us");
+    }
+
+
+    //takes the difference of the forces
+    public static double calcForce(Particle a, Particle b){
+        return a.pos-b.pos;
     }
 }
--- src/Particle.java
+++ src/Particle.java
@@ -1,53 +1,68 @@
 public class Particle {
-    int force = 1;
-    int acceleration = 0;
-    int velocity = 1;
+    double force = 1;
+    double acceleration = 0;
+    double velocity = 1;
     //int time = 0;
-    int pos = 0;
-    int mass = 1;
+    double pos = 0;
+    double mass = 1;
+    //double time = 0;
     boolean crash = false;
+    String name = "";
+
 
     public Particle(int _mass, int _pos){
         mass = _mass;
         pos = _pos;
     }
 
+    public Particle(int _mass, int _pos, String _name){
+        mass = _mass;
+        pos = _pos;
+        name = _name;
+    }
+
     //returns position after inputting change in time
-    public void UpdateState(){
-        UpdatePos();
-        UpdateVel();
-        UpdateAcc();
+    public void UpdateState(double time){
+        UpdateAcc(time);
+        UpdateVel(time);
+        UpdatePos(time);
         Crashed();
+        System.out.println(toString());
+
 
     }
 
-    public void UpdatePos(){
-        pos += velocity;
+    public void UpdatePos(double time){
+        pos += velocity*time;
     }
 
-    public void UpdateVel(){
-        velocity += acceleration;
+    //adjust for second object
+    public void UpdateVel(double time){
+        velocity += (acceleration/2)*time;
     }
 
-    public void UpdateAcc(){
-        acceleration = force/mass;
+    public void UpdateAcc(double time){
+        acceleration = (force/mass)*time;
         //  acceleration = forces/mass;
-        //newton 2nd law
+        //newton 2nd law F=ma
     }
+
+    public void UpdateForce(double n) {force = n;}
 
     public void Crashed(){
-        if(pos > 100){
+        if(pos > 100||pos<-100){
             crash = true;
         }
+        //set the boundaries
     }
 
     public String toString(){
 
         if(!crash){
-           return ("Position is " + pos);
+           return (name + "'s position is " + pos + " ");
         }
         else{
-            return "Particle has crashed";
+            return name + " has crashed";
         }
     }
 
